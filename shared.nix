8a86a3f448d327779753afb4a233dec1e2f73732
Konrad Borowski <konrad@borowski.pw>
2019-10-15 22:55:29 +0200

    Enable recommended settings

--- shared.nix
+++ shared.nix
@@ -2,4 +2,10 @@ with import <nixpkgs> {};
 
 {
   services.postgresql.package = pkgs.postgresql_11;
+  services.nginx = {
+    recommendedGzipSettings = true;
+    recommendedOptimisation = true;
+    recommendedProxySettings = true;
+    recommendedTlsSettings = true;
+  };
 }
