a4832339b7cf59d245a66fdb5a8d2d745a02f510
Konrad Borowski <konrad@borowski.pw>
2019-10-24 10:21:16 +0200

    Use Mozilla Intermediate TLS configuration

--- shared.nix
+++ shared.nix
@@ -6,8 +6,23 @@ with import <nixpkgs> {};
     recommendedGzipSettings = true;
     recommendedOptimisation = true;
     recommendedProxySettings = true;
-    recommendedTlsSettings = true;
+    sslCiphers = builtins.concatStringsSep ":" [
+      "ECDHE-ECDSA-AES128-GCM-SHA256"
+      "ECDHE-RSA-AES128-GCM-SHA256"
+      "ECDHE-ECDSA-AES256-GCM-SHA384"
+      "ECDHE-RSA-AES256-GCM-SHA384"
+      "ECDHE-ECDSA-CHACHA20-POLY1305"
+      "ECDHE-RSA-CHACHA20-POLY1305"
+      "DHE-RSA-AES128-GCM-SHA256"
+      "DHE-RSA-AES256-GCM-SHA384"
+    ];
     appendHttpConfig = ''
+      ssl_session_timeout 1d;
+      ssl_session_cache shared:MozSSL:10m;
+      ssl_session_tickets off;
+      ssl_prefer_server_ciphers off;
+      ssl_stapling on;
+      ssl_stapling_verify on;
       add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
     '';
   };
