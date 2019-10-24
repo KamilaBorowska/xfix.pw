17065f639283bf694f509829a4f4ffd53e9c4b77
Konrad Borowski <konrad@borowski.pw>
2019-10-24 11:23:28 +0200

    Use X-Content-Type-Options: nosniff

--- shared.nix
+++ shared.nix
@@ -24,6 +24,7 @@ with import <nixpkgs> {};
       ssl_stapling on;
       ssl_stapling_verify on;
       add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload" always;
+      add_header X-Content-Type-Options nosniff always;
     '';
   };
 }
