5e50eeaf9c6b3dd9929ca76c2357e8779563e53e
Konrad Borowski <konrad@borowski.pw>
2019-10-28 16:12:07 +0100

    Support ONLY Curve25519
    Who needs other methods anyway.

--- shared.nix
+++ shared.nix
@@ -2,7 +2,10 @@ with import <nixpkgs> {};
 
 {
   boot.tmpOnTmpfs = true;
-  services.openssh.extraConfig = "AuthenticationMethods publickey";
+  services.openssh = {
+    kexAlgorithms = [ "curve25519-sha256@libssh.org" ];
+    extraConfig = "AuthenticationMethods publickey";
+  };
   services.postgresql.package = pkgs.postgresql_11;
   services.nginx = {
     recommendedGzipSettings = true;
