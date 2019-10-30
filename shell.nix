80175e62f0cbf4c6358d323df9b3b3290092f38e
Konrad Borowski <konrad@borowski.pw>
2019-10-30 15:52:08 +0100

    Update nixpkgs revision

--- shell.nix
+++ shell.nix
@@ -3,7 +3,7 @@ with import <nixpkgs> {};
 stdenv.mkDerivation rec {
   name = "nixops-shell";
   buildInputs = [ nixops ];
-  revision = "c75de8bc12cc7e713206199e5ca30b224e295041";
+  revision = "12f5d47ef277ba3106a50f91d41b3750cf68f8e7";
   shellHook = ''
     export NIX_PATH="nixpkgs=https://github.com/NixOs/nixpkgs/archive/${revision}.tar.gz"
   '';
