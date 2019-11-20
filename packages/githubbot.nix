542a993e8b89ca5b4525464c5c61988cc4059210
Konrad Borowski <konrad@borowski.pw>
2019-11-20 16:05:03 +0100

    Build pastebin.run without GitLab CI

--- packages/githubbot.nix
+++ packages/githubbot.nix
@@ -1,4 +1,4 @@
-(import ./githubbot {})."github-bot-github:smogon/GitHub-Bot".override {
+(import ./nodejs {})."github-bot-github:smogon/GitHub-Bot".override {
   postInstall = ''
     npm run build
     # Not sure why the JSON file is not getting copied
