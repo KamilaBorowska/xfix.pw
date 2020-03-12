1b9c2bf6eb0dbf5bf4583be6aa4cbdf84f1999eb
Konrad Borowski <konrad@borowski.pw>
2020-03-13 00:19:04 +0100

    Update pulldown-cmark-babelmark

--- packages/babelmark.nix
+++ packages/babelmark.nix
@@ -1,14 +1,14 @@
 with import <nixpkgs> {};
 
-rustPlatform.buildRustPackage {
+rustPlatform.buildRustPackage.override { rustc = rustc_1_41; } {
   name = "pulldown-cmark-babelmark";
 
   src = fetchFromGitLab {
     owner = "KonradBorowski";
     repo = "pulldown-cmark-babelmark";
-    rev = "9cb8d814dfb3e058f0c086b19514811ef1bced8e";
-    sha256 = "1dsfd8nlbqd2pr0nk8g73bkvab6j2p0wixxv6ac54fxfp0lh1hb2";
+    rev = "17bedafc181e688c137c0efff914fcbad9b877ae";
+    sha256 = "0qz9l50fjazkkgbwj2cza22aaprh6bh6pbhw5aygg2lw147bw7rc";
   };
 
-  cargoSha256 = "1zrlj0llcxdanm2s4lsc3gjx2vd1fm5jj3j6db26wnz711kxlh5j";
+  cargoSha256 = "0882h3d1c6yiamzmw5mqyvivh3lhbsmrd7z7cc1p8z9a3f9ajl4y";
 }
