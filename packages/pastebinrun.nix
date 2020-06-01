7e2ca19e24b15a0f9f0e539edb12c7700a6f9930
Konrad Borowski <konrad@borowski.pw>
2020-06-01 18:01:44 +0200

    Update pastebin.run's Cargo hash

--- packages/pastebinrun.nix
+++ packages/pastebinrun.nix
@@ -17,7 +17,7 @@ rustPlatform.buildRustPackage {
   pname = "pastebinrun";
   version = "0.1.0";
   src = client-js-base.src;
-  cargoSha256 = "0kwwnk5sbzi29jrdm1h2mdmhmi03himam96lf7m8h7c4f25x8aq3";
+  cargoSha256 = "0k2y777wmh22lmm54w7v1agvz4kvqsn1cir4zbgmz6k6zy5azaif";
   nativeBuildInputs = [ pkgconfig ];
   buildInputs = [ openssl postgresql.lib ];
   patchPhase = ''
