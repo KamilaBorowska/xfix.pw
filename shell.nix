f881746b2f60bdf96336ab4d2d4735a416c97882
Konrad Borowski <konrad@borowski.pw>
2020-08-30 18:42:18 +0200

    Remove dev.nix and shell.nix

--- shell.nix
+++
@@ -1,9 +0,0 @@
-with import <nixpkgs> {};
-
-stdenv.mkDerivation rec {
-  name = "nixops-shell";
-  buildInputs = [ nixops ];
-  shellHook = ''
-    export NIX_PATH="nixpkgs=https://nixos.org/channels/nixos-20.03-small/nixexprs.tar.xz"
-  '';
-}
