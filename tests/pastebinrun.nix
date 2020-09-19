180379d41bd577626aa4fd9d48becb45583d89f5
Konrad Borowski <konrad@borowski.pw>
2020-09-19 20:09:54 +0200

    Rewrite pastebin.run test in Python

--- tests/pastebinrun.nix
+++ tests/pastebinrun.nix
@@ -1,14 +1,11 @@
-(import <nixpkgs/nixos/tests/make-test.nix> {
-    machine = {
-        imports = [../services/pastebinrun.nix];
-        systemd.services.postgresql.serviceConfig.TimeoutStartSec = 600;
-    };
+import <nixpkgs/nixos/tests/make-test-python.nix> {
+  machine = ../services/pastebinrun.nix;
 
-    testScript = ''
-        $machine->waitForOpenPort(8080);
-        my $id = $machine->succeed("curl -X POST 127.0.0.1:8080/api/v1/pastes --data 'code=Hello, world!'");
-        $machine->succeed("curl 127.0.0.1:8080/$id.txt") =~ /Hello, world!/ or die;
-    '';
-} {}).overrideAttrs(old: {
-    requiredSystemFeatures = ["nixos-test"];
-})
+  testScript = ''
+    machine.wait_for_open_port(8080)
+    id = machine.succeed(
+        "curl -X POST 127.0.0.1:8080/api/v1/pastes --data 'code=Hello, world!'"
+    )
+    assert "Hello, world!" in machine.succeed("curl 127.0.0.1:8080/{}.txt".format(id))
+  '';
+}
