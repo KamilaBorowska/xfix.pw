d412f273a3c7723ddba781e2dff54ce1bf6b8264
Konrad Borowski <konrad@borowski.pw>
2020-09-19 14:22:19 +0200

    Use daily backups only for automatic backups

--- services/zfsbackup.nix
+++ services/zfsbackup.nix
@@ -1,17 +1,20 @@
 { pkgs, ... }:
 {
   systemd.services.zfsbackup = {
-    path = [ pkgs.zfs ];
     serviceConfig = {
       EnvironmentFile = "/var/lib/keys/zfsbackup";
       Type = "oneshot";
-      ExecStart = "${pkgs.zfsbackup}/bin/zfsbackup-go send --fullIfOlderThan 360h --replication rpool/root/var b2://xfix-pw";
     };
+    path = [ pkgs.zfs pkgs.zfsbackup ];
+    script = ''
+      newest_snapshot="$(zfs list -H -p -t snapshot -o name,creation -S creation rpool/root/var | grep daily | head -1 | cut -f 1)"
+      zfsbackup-go send --fullIfOlderThan 360h --replication "$newest_snapshot" b2://xfix-pw
+    '';
   };
 
   systemd.timers.zfsbackup = {
     timerConfig = {
-      OnCalendar = "0,12:05";
+      OnCalendar = "0:05";
       Persistent = "true";
     };
     wantedBy = [ "multi-user.target" ];
