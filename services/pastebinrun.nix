674c741439f57d02c1ea21e85db6ae17b1db2b0f
Konrad Borowski <konrad@borowski.pw>
2020-10-11 19:20:28 +0200

    Disable maximum expiration timestamp for pastebin.run

--- services/pastebinrun.nix
+++ services/pastebinrun.nix
@@ -11,9 +11,6 @@ in
         locations."/static".root = pastebinrun;
         locations."/static/js" = {
           root = pastebinrun;
-          extraConfig = ''
-            expires max;
-          '';
         };
       };
       "www.pastebin.run" = {
