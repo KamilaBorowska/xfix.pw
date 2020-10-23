3dc356d22f02c81a6d8446663ed26885d43051d8
Konrad Borowski <konrad@borowski.pw>
2020-10-23 12:27:42 +0200

    Avoid explicitly storing hashes for locked dependencies in repository

--- packages/babelmark.nix
+++ packages/babelmark.nix
@@ -1,14 +1,15 @@
-with import <nixpkgs> {};
-
-rustPlatform.buildRustPackage {
+let
+  pkgs = import <nixpkgs> {};
+  sources = import ../nix/sources.nix;
+  naersk = pkgs.callPackage sources.naersk { };
+in
+naersk.buildPackage {
   name = "pulldown-cmark-babelmark";
 
-  src = fetchFromGitLab {
+  src = pkgs.fetchFromGitLab {
     owner = "KonradBorowski";
     repo = "pulldown-cmark-babelmark";
     rev = "e15ad53616625b2b142c07dbe3878477b1bd63e3";
     sha256 = "16qinliscmrlj8r1nz0ky8nmckkq64v1l6wr3sjqsi3sr2g70gyk";
   };
-
-  cargoSha256 = "1jhf8rngxij88bigj1v03w1hs03w5j19lvvbb5v67qaialkfaghd";
 }
