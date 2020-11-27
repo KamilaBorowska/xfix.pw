27a4971e217569e3b722e2e3eaed752b816ca367
Konrad Borowski <konrad@borowski.pw>
2020-11-27 23:29:31 +0100

    Update psdevbot-rust

--- packages/psdevbot-rust.nix
+++ packages/psdevbot-rust.nix
@@ -7,8 +7,8 @@ naersk.buildPackage {
   src = pkgs.fetchFromGitLab {
     owner = "KonradBorowski";
     repo = "psdevbot-rust";
-    rev = "45b5bf32bf3d4a8c08d23de730fbd9da118ea1fc";
-    sha256 = "0ksa31k4kl9lkkq62q5cf2gvipm40grabxw6igmb16p8qyzj5vgd";
+    rev = "c55777034c830593fbb26cffdb8056a6bed2a636";
+    sha256 = "1bnvd5nh18ip3mr4s43m526cpbm2sadh586m61f7vbrigdyarias";
   };
 
   nativeBuildInputs = [ pkgs.pkgconfig ];
