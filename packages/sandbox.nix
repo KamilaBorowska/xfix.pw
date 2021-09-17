db18785a4a3ed0d92b77e0cab9e0deeac6f0a586
Konrad Borowski <konrad@borowski.pw>
2021-09-17 15:29:51 +0200

    Update nix-based-sandbox

--- packages/sandbox.nix
+++ packages/sandbox.nix
@@ -7,7 +7,7 @@ naersk.buildPackage {
   src = pkgs.fetchFromGitLab {
     owner = "pastebinrun";
     repo = "nix-based-sandbox";
-    rev = "89e22555e1f1eb0fce16de4c3592cfcb555cfc59";
-    sha256 = "13fdfnmxx87hwh457jzzl7p9rvh7nnpifhz1a78cvzv65wvmr3hc";
+    rev = "41e281de413bdef5f5c1e6bcd09355fb5ed7038c";
+    sha256 = "0zcs5yfah8f8nlpsfbhdyklcgqdqi4pr8sh37prdgq2sdv1rw2s5";
   };
 }
