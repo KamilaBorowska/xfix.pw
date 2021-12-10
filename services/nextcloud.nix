42fcd878dceec807633158f6536e9591837ffb5d
Konrad Borowski <konrad.borowski@kontomatik.com>
2021-12-10 15:26:56 +0100

    Upgrade Nextcloud to 23

--- services/nextcloud.nix
+++ services/nextcloud.nix
@@ -11,7 +11,7 @@
       adminuser = "xfix";
       adminpassFile = toString (pkgs.writeText "password" "password");
     };
-    package = pkgs.nextcloud22;
+    package = pkgs.nextcloud23;
   };
   services.postgresql = {
     enable = true;
