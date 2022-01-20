ec18734de826ffa2cec0904ee912c027ee9f435f
Konrad Borowski <konrad@borowski.pw>
2022-01-20 16:44:21 +0100

    Revert "Use TLSv1.3"
    This breaks babelmark.
    
    This reverts commit f572a58d4d4882db4de9fc62b0e1b279959f8411.

--- services/nginx.nix
+++ services/nginx.nix
@@ -5,12 +5,20 @@
     recommendedGzipSettings = true;
     recommendedOptimisation = true;
     recommendedProxySettings = true;
-    sslCiphers = null;
+    sslCiphers = builtins.concatStringsSep ":" [
+      "ECDHE-ECDSA-AES128-GCM-SHA256"
+      "ECDHE-RSA-AES128-GCM-SHA256"
+      "ECDHE-ECDSA-AES256-GCM-SHA384"
+      "ECDHE-RSA-AES256-GCM-SHA384"
+      "ECDHE-ECDSA-CHACHA20-POLY1305"
+      "ECDHE-RSA-CHACHA20-POLY1305"
+      "DHE-RSA-AES128-GCM-SHA256"
+      "DHE-RSA-AES256-GCM-SHA384"
+    ];
     sslDhparam = pkgs.fetchurl {
       url = "https://ssl-config.mozilla.org/ffdhe2048.txt";
       sha256 = "08dpmhxn8bmmhv7lyd7fxgih2xv4j4xanf5w3pfd0nhqcf2pbxrf";
     };
-    sslProtocols = "TLSv1.3";
     appendHttpConfig = ''
       ssl_session_timeout 1d;
       ssl_session_cache shared:MozSSL:10m;
@@ -18,7 +26,9 @@
       ssl_prefer_server_ciphers off;
       ssl_stapling on;
       ssl_stapling_verify on;
+      ssl_early_data on;
       add_header Strict-Transport-Security "max-age=63072000; includeSubDomains; preload";
+      add_header X-Content-Type-Options nosniff always;
     '';
   };
 
