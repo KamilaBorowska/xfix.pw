23b8d0446adec59cb73b30347d5f63577bec9872
Konrad Borowski <konrad@borowski.pw>
2022-01-20 16:45:17 +0100

    Update pulldown-cmark-babelmark

--- packages/babelmark.nix
+++ packages/babelmark.nix
@@ -7,7 +7,7 @@ naersk.buildPackage {
   src = pkgs.fetchFromGitLab {
     owner = "KonradBorowski";
     repo = "pulldown-cmark-babelmark";
-    rev = "78f5ab6d529547d41d1fad5f9ba611f9e87c6cce";
-    sha256 = "099db7rw8ll5yyp9b6ds1b022yidsw7zry24szc036lb8sy486pl";
+    rev = "24a597d9d7ea9a946dbab376ec179bb10a837b27";
+    sha256 = "1s15qawnji7plrdz26bqm9mh68m4k1djynl9cn8s9bzc8jnpdvs1";
   };
 }
