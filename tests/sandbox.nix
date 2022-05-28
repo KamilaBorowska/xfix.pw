6fcff527ac228cd506b6c02c637d0e91de272e09
Konrad Borowski <konrad@borowski.pw>
2022-05-28 23:33:31 +0200

    Fix files type in sandbox test

--- tests/sandbox.nix
+++ tests/sandbox.nix
@@ -4,7 +4,7 @@ import <nixpkgs/nixos/tests/make-test-python.nix> {
   testScript = ''
     machine.wait_for_open_port(8082)
     out = machine.succeed("""
-        curl --data '{"files": [], "stdin": "", "code": "echo Hello, world!"}' http://localhost:8082
+        curl --data '{"files": {}, "stdin": "", "code": "echo Hello, world!"}' http://localhost:8082
     """)
     assert "Hello, world!" in out
   '';
