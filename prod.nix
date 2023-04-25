a13d9ba455924c77993ffb8c5ca05a69fdf58986
Konrad Borowski <konrad@borowski.pw>
2023-04-25 14:04:21 +0200

    Add new SSH key

--- prod.nix
+++ prod.nix
@@ -39,6 +39,7 @@
     shell = pkgs.fish;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJ7GBke6Y5m3z4VtiQPzQjVGnPLi6/XwFMNuNtVnL6vM"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBuur6VR3/FhTltKKL3k+t5OgJfmFR9NPOGs3dEychPR"
     ];
   };
 
