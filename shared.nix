9449acae12669c760175e8eb1fec47cef0d44a9e
Konrad Borowski <konrad@borowski.pw>
2023-06-11 12:22:16 +0200

    Rename KexAlgorithms option

--- shared.nix
+++ shared.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 {
   services.openssh = {
-    kexAlgorithms = [ "curve25519-sha256@libssh.org" ];
+    settings.KexAlgorithms = [ "curve25519-sha256@libssh.org" ];
     extraConfig = "AuthenticationMethods publickey";
   };
   services.postgresql.package = pkgs.postgresql_14;
